42c54ab93e0966d31d3b3b8ccd670a2a86f485e0
Ashford <ashfordrebelo147@gmail.com>
2024-04-06 18:54:26 +0530

    new not founf page

--- finalyear-frontend/src/app/not-found.tsx
+++ finalyear-frontend/src/app/not-found.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import styles from './NotFound.module.css'; // Import your CSS module
+
 export default function NotFound(){
     return (
-        <div>
-            <h2>Page could not be found</h2>
-            <p>Try Again</p>
-            <Link href="/">Return Home</Link>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Page not found</h2>
+            <p className={styles.message}>Oops! Looks like the page you're looking for doesn't exist.</p>
+            <img src="Images/notfound.gif" alt="Page not found" className={styles.gif} />
+            <Link href="/Home">
+                <span className={styles.link}>Return Home</span>
+            </Link>
         </div>
     );
 }
